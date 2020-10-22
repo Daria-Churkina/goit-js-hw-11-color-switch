0e9141aa40f4536d4c6d9d98abdf596172abc9ba
Daria-Churkina <darka.yaremenko@gmail.com>
2020-10-22 20:57:11 +0300

    добавляет решение по смене цвета

---
+++ goit-js-hw-11-color-switch/src/index.js
@@ -0,0 +1,43 @@
+import './styles.css';
+
+const colors = [
+  '#FFFFFF',
+  '#2196F3',
+  '#4CAF50',
+  '#FF9800',
+  '#009688',
+  '#795548',
+];
+
+const randomIntegerFromInterval = (min, max) => {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
+
+const refs = {
+  bodyContent: document.querySelector('body'),
+  startBtn: document.querySelector('button[data-action="start"]'),
+  stopBtn: document.querySelector('button[data-action="stop"]'),
+  isActive: false,
+  colorGenerate: null,
+};
+
+refs.startBtn.addEventListener('click', handleStartColorGenerateClick);
+refs.stopBtn.addEventListener('click', handleStopColorGenerateClick);
+
+function handleStartColorGenerateClick() {
+  if (refs.isActive) {
+    return;
+  }
+
+  refs.colorGenerate = setInterval(() => {
+    refs.isActive = true;
+
+    const colorRandom = randomIntegerFromInterval(0, colors.length);
+    refs.bodyContent.style.backgroundColor = colors[colorRandom];
+  }, 1000);
+}
+
+function handleStopColorGenerateClick() {
+  clearInterval(refs.colorGenerate);
+  refs.isActive = false;
+}
